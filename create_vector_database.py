15fe17caffe916ca25796c829955269279d8e558
JATH <jath@danskespil.dk>
2023-08-31 14:44:29 +0200

    Added new vector databases

--- create_vector_database.py
+++ create_vector_database.py
@@ -14,15 +14,41 @@ idx = 0
 text_dict = {}
 for i in range(2001, 2023):
     with open(f'/app/queens_speeches/speeches/{i}.txt') as f:
-        for line in f.readlines():
+        total_text = f"År {i}"
+        for line_idx, line in enumerate(f.readlines()):
+            total_text += '\n' + line
             line = line.strip()
             if len(line)>0:
-                line = f'År: {i}\n' +  line
+                line = f'År: {i}\n Linjenummer: {line_idx} \n' +  line
                 embedding = openai.Embedding.create(input=line, engine='text-embedding-ada-002')['data'][0]['embedding']
                 embedding = np.array(embedding, dtype=EMBEDDING_DTYPE)
                 t.add_item(idx, embedding)
                 text_dict[idx] = line
                 idx += 1
+    selected_conversation_hist = [
+        {"role": "system",
+         "content": f"""
+            Your job is to summarize the Danish Queens new years speeches.
+            Please include information such as but not limited to 
+            themes, people, countries, facts, events and minorities.
+            Please do so in Danish.
+             """},
+    ]
+    modified_question = "Opsummer følgende tale: \n" + \
+                        total_text
+    # Add question to conversation
+    messages = selected_conversation_hist + [{"role": "user", "content": modified_question}]
+
+    raw_answer = openai.ChatCompletion.create(
+        engine="gpt-35-turbo",
+        messages=messages
+    )
+    answer = raw_answer["choices"][0]["message"]["content"]
+    embedding = openai.Embedding.create(input=answer, engine='text-embedding-ada-002')['data'][0]['embedding']
+    embedding = np.array(embedding, dtype=EMBEDDING_DTYPE)
+    t.add_item(i, embedding)
+    text_dict[idx] = answer
+    idx += 1
     print(i)
 t.build(10) # 10 trees
 t.save('queen_speeches.ann')
